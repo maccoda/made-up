8719cc8fd7d95a553c082886740da8ca9a6d8098
Dylan Maccora <maccora17@gmail.com>
2017-04-24 22:56:22 +1000

    Initial commit

---
+++ resources/all_test.md
@@ -0,0 +1,44 @@
+# The heading
+## Some stuff
+* Point one
+* Point two
+    * Indent point one
+* Point three
+
+## Here is an image
+![ferris](rustacean-orig-noshadow.png)
+
+## Now for some code
+### Inline
+We first want to show the `main` function with `"Hello World"`
+
+### Block
+```rust
+fn main() {
+    println!("Hello World");
+}
+```
+
+
+## Links
+### Links to external pages
+This is the author [maccoda](https://github.com/maccoda)
+
+### Internal links on page
+Lets go [back](#here-is-an-image) to the pictures
+
+## Fonts
+We can have **bold** text, *italic* text
+
+<!-- Tables are not recognized by CommonMark it appears -->
+
+## Tables
+| Column 1 | Column 2 |
+| --- | --- |
+|data | more data |
+| even more | something |
+
+
+## A second page
+Want to be able to link to [another page](second-page.md) so that is is similar to
+a static site.
---
+++ src/html.rs
@@ -0,0 +1,121 @@
+use pulldown_cmark::{Event, Tag};
+
+trait ToHtml {
+    fn to_html() -> String;
+}
+
+
+#[derive(Debug, Clone)]
+struct Element<'a> {
+    tag: Tag<'a>,
+    content: Option<String>,
+}
+
+impl<'a> Element<'a> {
+    fn new(tag: Tag<'a>) -> Element<'a> {
+        Element {
+            tag: tag,
+            content: None,
+        }
+    }
+
+    fn content(&mut self, content: Option<String>) {
+        self.content = content;
+    }
+}
+
+fn name_to_id(name: &String) -> String {
+    name.to_lowercase().replace(" ", "-")
+}
+
+#[derive(Debug)]
+pub struct Consumer<'a, I> {
+    iter: I,
+    buffer: String,
+    current: Option<Tag<'a>>,
+}
+impl<'a, I: Iterator<Item = Event<'a>>> Consumer<'a, I> {
+    fn consume<'b>(&mut self) -> String {
+        while let Some(event) = self.iter.next() {
+            println!("{:?}", self.buffer);
+            println!("{:?}", event);
+            match event {
+                Event::Start(tag) => {
+                    self.buffer.push_str(&print_start_elem(&tag));
+                    self.current = Some(tag);
+                }
+                Event::End(tag) => self.buffer.push_str(&print_end_elem(&tag)),
+                Event::Text(text) => {
+                    if let Some(tag) = self.current.clone() {
+                        match tag {
+                            Tag::Header(_) => {
+                                let to_add =
+                                    format!("{}\"> {}", name_to_id(&text.to_string()), text);
+                                self.buffer.push_str(&to_add)
+                            }
+                            Tag::Image(_, _) => self.buffer.push_str(&format!(" alt=\"{}\"", text)),
+                            _ => self.buffer.push_str(&text),
+                        }
+                    } else {
+                        self.buffer.push_str(&text)
+                    }
+                }
+                elem => println!("Unhandled type: {:?}", elem),
+            }
+        }
+        self.buffer.clone()
+    }
+}
+fn print_start_elem(tag: &Tag) -> String {
+    let result = match tag {
+        &Tag::Header(int) => format!("<h{} id=\"", int),
+        &Tag::Strong => "<b>".to_string(),
+        &Tag::Emphasis => "<em>".to_string(),
+        &Tag::Item => "<li>".to_string(),
+        &Tag::List(_) => "<ul>".to_string(),
+        &Tag::Paragraph => "<p>".to_string(),
+        &Tag::Image(ref src, _) => format!("<img src=\"{}\"", src.as_ref()),
+        &Tag::Code => "<code>".to_string(),
+        &Tag::CodeBlock(ref lang) => format!("<pre><code class=\"language-{}\">", lang),
+        &Tag::Link(ref href, _) => format!("<a href=\"{}\">", href),
+        tag => {
+            println!("{:?}", tag);
+            unimplemented!();
+        }
+    };
+
+    println!("{:?}", result);
+    result
+}
+
+fn print_end_elem(tag: &Tag) -> String {
+    let result = match tag {
+        &Tag::Header(int) => format!("</h{}>\n", int),
+        &Tag::Strong => "</b>\n".to_string(),
+        &Tag::Emphasis => "</em>\n".to_string(),
+        &Tag::Item => "</li>\n".to_string(),
+        &Tag::List(_) => "</ul>\n".to_string(),
+        &Tag::Paragraph => "</p>\n".to_string(),
+        &Tag::Image(_, _) => "/>".to_string(),
+        &Tag::Code => "</code>".to_string(),
+        &Tag::CodeBlock(_) => "</code></pre>".to_string(),
+        &Tag::Link(_, _) => "</a>".to_string(),
+        tag => {
+            println!("{:?}", tag);
+            unimplemented!();
+        }
+    };
+
+    println!("{:?}", result);
+    result
+}
+
+pub fn consume<'a, I: Iterator<Item = Event<'a>>>(iter: I) -> String {
+    let mut consumer = Consumer {
+        iter: iter,
+        buffer: String::new(),
+        current: None,
+    };
+
+    consumer.consume()
+}
