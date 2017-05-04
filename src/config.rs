e1836538d73be96d889a3bdaa435133aa74e48fe
Dylan Maccora <maccora17@gmail.com>
2017-05-04 22:41:38 +1000

    Initial step of extra config handling

--- src/config.rs
+++ src/config.rs
@@ -5,16 +5,24 @@ use super::file_utils;
 
 // TODO Look into what we actully need here
 #[derive(Serialize, Deserialize, Debug)]
-pub struct Configuration {
+pub struct RawConfiguration {
     stylesheet: Option<String>,
     index: Option<bool>,
     out_dir: Option<String>,
 }
 
-impl Configuration {
-    pub fn from<P: AsRef<Path>>(path: P) -> Configuration {
+// TODO Add to this and ensure that the RawConfiguration is kept private
+// Mirror of `RawConfiguration` but has resolved all `Option`s.
+pub struct Configuration {
+    stylesheet: String,
+    index: bool,
+    out_dir: String,
+}
+
+impl RawConfiguration {
+    pub fn from<P: AsRef<Path>>(path: P) -> RawConfiguration {
         // TODO Add error handling
-        debug!("Reading configuration file");
+        debug!("Reading Rawconfiguration file");
         let config = serde_yaml::from_str(&file_utils::read_from_file(path)).unwrap();
         info!("{:?}", config);
         config
@@ -33,17 +41,17 @@ impl Configuration {
 
 #[cfg(test)]
 mod tests {
-    use super::Configuration;
+    use super::RawConfiguration;
     #[test]
     fn test_read() {
-        let actual = Configuration::from("tests/resources/test_conf.yml");
+        let actual = RawConfiguration::from("tests/resources/test_conf.yml");
         assert_eq!(actual.stylesheet, Some("test_style.css".to_string()));
         assert_eq!(actual.index, None);
     }
 
     #[test]
     fn test_read_all() {
-        let actual = Configuration::from("tests/resources/test_conf_all.yml");
+        let actual = RawConfiguration::from("tests/resources/test_conf_all.yml");
         assert_eq!(actual.stylesheet, Some("style.css".to_string()));
         assert_eq!(actual.index, Some(false));
         assert_eq!(actual.out_dir, Some("output".to_string()));
